1f2533ffaef356cc1c556db7a1fc320bca6e0786
ShahidNadeem <shahid.nadeem@mobilezapp.com>
2013-03-16 11:19:27 +0500

    Upload the design theme 1 files

---
+++ gocart/themes/public/views/mainHeader.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+-->    
+    <title>Matrix - Responsive Tile-Based Template</title>
+    <?php echo theme_css('style.css', true); ?>
+    <?php echo theme_css('style-blue.css', true);?>
+     <?php echo theme_js('scripts/jquery.min.js', true);?> 
+    <?php echo theme_js('scripts/jquery.masonry.min.js', true);?>   
+    <?php echo theme_js('scripts/jquery.easing.1.3.js', true);?>   
+    <?php echo theme_js('scripts/jquery.masonry.min.js', true);?>   
+    <?php echo theme_js('scripts/MetroJs.lt.js', true);?>   
+    <?php echo theme_js('scripts/jquery.fancybox-1.3.4.js', true);?>   
+    <?php echo theme_js('scripts/jquery.flexslider-min.js', true);?>   
+    <?php echo theme_js('scripts/hoverintent.js', true);?>   
+    <?php echo theme_js('scripts/jquery.jplayer.min.js', true);?>   
+    <?php echo theme_js('scripts/organictabs.jquery.js', true);?>   
+
+    <!--[if lt IE 9]>
+    <style type="text/css">
+    @import url(<?php echo theme_css('style-ie8.css', true);?> );
+    </style>
+    <script src="scripts/css3-mediaqueries.js" type="text/javascript" charset="utf-8"></script>
+    <script>
+    document.createElement('header');
+    document.createElement('nav');
+    document.createElement('section');
+    document.createElement('article');
+    document.createElement('aside');
+    document.createElement('footer');
+    document.createElement('hgroup');
+    </script>
+    <![endif]-->
+    <?php echo theme_js('scripts/javascript.js', true);?>   
+    <?php echo theme_js('scripts/mediaplayer.js', true);?>   
+
+</head>
