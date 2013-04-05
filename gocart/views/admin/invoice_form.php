0c76886f5edb455ca5f5b56b6f876b069f743280
Junaidkhalil <khalil.junaid@gmail.com>
2013-04-05 09:38:54 +0500

    Commit Again

--- gocart/views/admin/invoice_form.php
+++ gocart/views/admin/invoice_form.php
@@ -1,8 +1,8 @@
-<?php define('ADMIN_FOLDER', $this->config->item('admin_folder')); ?> 
 <div id="main" style="min-height:1000px">
 <div class="container">
-    <? include_once('includes/admin_profile.php');?> 
-    <!-- End top-right -->
+                <? include_once('includes/admin_profile.php');?>    
+        <!-- End top-right -->
+    </div>
     <div id="main_container">
         <div class="row-fluid">
             <div class="span12">
@@ -13,37 +13,32 @@
                             <h4> Invoice form</h4>
                         </div>
                     </div>
-                    <!-- End .title -->  
+
                     <div class="content">
 
-                        <?php echo form_open_multipart($this->config->item('admin_folder').'/invoices/invoices_save/'); ?>
+                        <?php echo form_open_multipart($this->config->item('admin_folder').'/invoices/form/'.$id); ?>
 
                         <div class="tab-content">
                             <div class="tab-pane active" id="description_tab">
 
                                 <fieldset>
-
-                                                                                    
-
-                                    <label for="name">Client Name</label>
+                                    <label for="name"><?php echo lang('name');?></label>
                                     <?php
-                                        $data    = array('name'=>'name','id'=>'name', 'value'=>set_value('name','name'), 'class'=>'span12');
+                                        $data    = array('name'=>'name', 'value'=>set_value('name', $name), 'class'=>'span12');
                                         echo form_input($data);
                                     ?>
 
-                                    <label for="group">Group</label>
+                                    <label for="description"><?php echo lang('description');?></label>
                                     <?php
-                                        $data    = array('name'=>'client', 'value'=>set_value('group'), 'class'=>'span12');
-                                        echo form_input($data);
+                                        $data    = array('name'=>'description', 'class'=>'redactor', 'value'=>set_value('description', $description));
+                                        echo form_textarea($data);
                                     ?>
 
-                                    <label for="date">Date </label>
+                                    <label for="date"> Date </label>
                                     <?php
-                                        $data    = array('name'=>'date','id'=>'datepicker1', 'value'=>set_value('date'), 'class'=>'span12');
+                                        $data    = array('name'=>'name', 'value'=>set_value('name', $name), 'class'=>'span12');
                                         echo form_input($data);
-                                    ?> 
-                                     
-                                   
+                                    ?>
                                 </fieldset>
 
                             </div>   
