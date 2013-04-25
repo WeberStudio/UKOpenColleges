d870068a55dfaeb22d6ec790d557bc1b4d766fd2
navera <navera.aziz@weber.com>
2013-04-25 17:57:22 +0500

    front end commit categories

---
+++ gocart/themes/oneTouch/views/leftPanel.php
@@ -0,0 +1,153 @@
+<div id = "custom-style-wrapper">
+    <div id="tempate-switcher" style="display: none;">
+
+        <div class="option-title">
+            <span>Choose site template:</span>
+        </div>
+
+        <div class="template-switch">
+            <a class="template-option" href = "" data-size="normal"><img src="http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/checkbox_2.png" alt="">Usual</a>
+            <a class="template-option" href = "http://theme.crumina.net/onetouch/magazine-layout/" data-size="magazine"><img src="http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/checkbox_2.png" alt="">Magazine</a>
+            <a class="template-option" href = "http://theme.crumina.net/onetouch/corporate-layout/" data-size="corporate"><img src="http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/checkbox_2.png" alt="">Corporate</a>
+        </div>
+
+
+    </div>
+
+    <div id="custom-style" style="display: none;">
+
+        <div class="option-title">
+            <span>
+                Choose background:
+            </span>
+        </div>
+
+        <ul class="background-select">
+            <li data-selector = "#body-wrapper"><span class = "bg-title">16 Content BG textures</span>
+                <span class="float-right">
+                    <a class="pattern-example color"><img src="<?php echo theme_img("custom-slider-img/color.png")?>" alt="" /></a>
+                    <a class="pattern-example image"><img src="<?php echo theme_img("custom-slider-img/title-icon.png")?>" alt="" /></a></span>
+                <div class="pattern-select-wrapper">
+                    <div class="pattern-select">
+                        <div class="pattern-example pic"><img src="<?php echo theme_img("custom-slider-img/furley_bg.png")?>" alt = "" /> 
+                        </div>
+                        <div class="pattern-example pic">
+            <img src="<?php echo theme_img("custom-slider-img/2.jpg")?>" alt = "" />
+                        </div>
+                        <div class="pattern-example pic"><img src="<?php echo theme_img("custom-slider-img/1.jpg")?>" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src="<?php echo theme_img("custom-slider-img/city.jpg")?>" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/3.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/tileable_wood_texture.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/vertical_cloth.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/bg.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/purty_wood.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/scribble_light.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/brushed_alu.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/hexellence.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/darkdenim3.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/111.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/diamond_upholstery2.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/egg_shell.png" alt = "" /> </div>
+                </div></div>            </li>
+            <li data-selector = "#darkf"><span class = "bg-title">16 Footer BG textures</span> 
+                <span class="float-right"><a class="pattern-example color">
+                        <img src="<?php echo theme_img("custom-slider-img/color.png")?>" alt="" /></a><a class="pattern-example image">
+                        <img src="images/custom-slider-img/title-icon.png" alt="" /></a></span>
+
+                <div class="pattern-select-wrapper"><div class="pattern-select"><div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/furley_bg.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/2.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/1.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/city.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/3.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/tileable_wood_texture.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/vertical_cloth.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/bg.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/purty_wood.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/scribble_light.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/brushed_alu.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/hexellence.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/darkdenim3.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/111.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/diamond_upholstery2.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/egg_shell.png" alt = "" /> </div></div></div></li>
+            <li data-selector = "body"><span class = "bg-title">16 Body BG textures</span> <span class="float-right">
+                    <a class="pattern-example color">
+                        <img src="images/custom-slider-img/color.png" alt="" /></a>
+                    <a class="pattern-example image"><img src="images/custom-slider-img/title-icon.png" alt="" /></a></span>
+                <div class="pattern-select-wrapper"><div class="pattern-select"><div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/furley_bg.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/2.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/1.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/city.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/3.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/tileable_wood_texture.png" alt = "" /> </div><div class="pattern-example pic"><img src = "http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/pattern/thumb/vertical_cloth.png" alt = "" /> </div><div class="pattern-example pic"><img src = "http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/pattern/thumb/bg.jpg" alt = "" /> </div><div class="pattern-example pic"><img src = "http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/pattern/thumb/purty_wood.png" alt = "" /> </div><div class="pattern-example pic"><img src = "http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/pattern/thumb/scribble_light.png" alt = "" /> </div><div class="pattern-example pic"><img src = "http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/pattern/thumb/brushed_alu.png" alt = "" /> </div><div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/hexellence.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/darkdenim3.png" alt = "" /> </div>
+                        <div class="pattern-example pic">
+                        <img src = "images/custom-slider-img/pattern/thumb/111.jpg" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/diamond_upholstery2.png" alt = "" /> </div>
+                        <div class="pattern-example pic"><img src = "images/custom-slider-img/pattern/thumb/egg_shell.png" alt = "" /> </div>
+                    </div>
+            </div>            </li>
+        </ul>
+
+        <div class="option-title">
+            <span>
+                Choose layout:
+            </span>
+        </div>
+
+        <ul class="wrapper-switch" data-checkbox="http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/inc/custom_style/assets/img/">
+            <li><a class="boxed-switcher" data-wrap="no-wrap">
+                    <img src="images/custom-slider-img/checkbox_1.png" alt="">
+                    Wide layout 1200 px grid</a></li>
+            <li><a class="boxed-switcher" data-wrap="wrap-2">
+                    <img src="images/custom-slider-img/checkbox_2.png" alt="">Boxed layout 1200 px grid</a></li>
+        </ul>
+
+        <div class="option-title">
+            <span>
+                Choose menu:
+            </span>
+        </div>
+
+        <a class="menu-option" data-size="normal"><img src="images/custom-slider-img/checkbox_1.png" alt="">First variant</a>
+        <a class="menu-option" data-size="dropdown"><img src="images/custom-slider-img/checkbox_2.png" alt="">Second variant</a>
+
+        <div class="option-title">
+            <span>
+                Choose slider width:
+            </span>
+        </div>
+
+
+        <div class="slider-switch">
+            <a class="slider-switcher current" data-size="boxed"><img src="images/custom-slider-img/slider-1.png" alt=""></a>
+            <a class="slider-switcher" data-size="right"><img src="images/custom-slider-img/slider-2.png" alt=""></a>
+            <a class="slider-switcher" data-size="full"><img src="images/custom-slider-img/slider-3.png" alt=""></a>
+        </div>
+
+        <a href="" id="load_defaults_settings">Load Defaults</a>
+        <img id="appear_icon" src="images/custom-slider-img/settings.png" alt="" />
+        <div id="custom-style-colorpicker">
+        </div>
+    </div>
+</div>
---
+++ gocart/themes/oneTouch/views/mainHeader.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>OneTouch</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    
+
+    <link rel="icon" type="image/png" href="">
+    <link rel="alternate" type="application/rss+xml" title="OneTouch Feed" href="">
+    <?php echo theme_css('google-font.css', true); ?>
+      <?php echo theme_css('wo-commerence.css', true); ?>
+      <?php echo theme_css('aqpb-view.css', true); ?>
+      <?php echo theme_css('bootstrap.css', true); ?>
+      <?php echo theme_css('js_composer_front.css', true); ?>
+      <?php echo theme_css('revslider-settings.css', true); ?>
+      <?php echo theme_css('revslider-captions.css', true); ?>
+      <?php echo theme_css('frontend.css', true); ?>
+      <?php echo theme_css('foundation.min.css', true); ?>
+      <?php echo theme_css('app.css', true); ?>
+      <?php echo theme_css('shortcodes.css', true); ?>
+      <?php echo theme_css('responsive.css', true); ?>
+      <?php echo theme_css('colorbox.css', true); ?>
+
+    <!--add the serach classess-->
+    <?php echo theme_css('jquery.fancybox-1.3.1.css?c-v=24739', true); ?>
+    <?php echo theme_css('header.css?c-v=24739', true); ?>
+    
+    <script type="text/javascript">
+        // needed to check if the LoginConflictModal popup is displayed.
+        var isLoginConflited = 'False'
+    </script>
+     
+    <!--   End search     -->
+    <?php echo theme_js('js/jquery-1-8-3.js?c-v=24739', true);?>
+    <?php echo theme_js('js/ready2.js?c-v=24739', true);?>
+    <?php echo theme_js('js/jquery.plugins.min.js', true);?>
+    <?php echo theme_js('js/jquery.themepunch.plugins.min.js', true);?>
+    <?php echo theme_js('js/jquery.themepunch.revolution.min.js', true);?>
+    <?php echo theme_js('js/modernizr.foundation.js', true);?>
+    <?php echo theme_js('js/app.js', true);?>
+    <?php echo theme_js('js/modernizr.foundation.js', true);?>  
+
+
+    <!-- WooCommerce Version -->
+    <meta name="generator" content="WooCommerce 1.6.6" />
+
+    
+
+    <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <link href='css/ie.css' rel='stylesheet' type='text/css'>
+    <![endif]-->
+
+
+    <?php echo theme_css('options.css', true); ?>
+   
+
+    <style>
+        #body-wrapper {
+            background-attachment:fixed!important;
+            background-repeat:repeat;
+        }
+    </style>
+
+    <style type="text/css">
+        .facebookOuter {
+            background-color: transparent;
+            padding-bottom: 1px;
+            border: 1px solid #687f8e;
+        }
+        .facebookInner {
+            height:275px;
+            border:none;
+            background: transparent;
+        }
+    </style>
+</head>
