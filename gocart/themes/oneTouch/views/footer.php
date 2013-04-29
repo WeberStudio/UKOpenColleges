58ef6cd033f350b2a3957e23e861e924e8d64c02
Junaid Khalil <j.khalil@weprosolutions.co.uk>
2013-04-29 10:30:32 +0500

    Footer changes

--- gocart/themes/oneTouch/views/footer.php
+++ gocart/themes/oneTouch/views/footer.php
@@ -1,181 +1,104 @@
 <div id="darkf">
-    <section id="footer" role="contentinfo">
-
-        <div class="row">
-
-            <div class="five columns">
-                <section id="crum_latest_tweets-2" class="widget-1 widget-first widget twitter-widget">
-                    <div class="widget-inner">
-                        <div class="subtitle">Our latest twitter news</div><h3>Latest Tweets</h3><div class='tweet'>Crumina: Check this! Great #themeforest item 'One Touch - Multifunctional Metro Stylish Theme'
-                           
-                            <div class='time'>77 days ago</div></div>
-							<div class='tweet'>Crumina: BGs <div class='time'>105 days ago</div></div>
-							<div class='tweet'>Crumina: Check this! Great creative Photo and video WP Theme - Boson.  
-							<div class='time'>111 days ago</div></div></div></section>
-			</div>
-            <div class="five columns">
-                <section id="recent_posts-2" class="widget-1 widget-first widget recent-posts-widget"><div class="widget-inner"><div class="subtitle">
-                            Some latest news</div><h3>Recent posts</h3>
-
-
-                        <article class="mini date">
-
-                            <time datetime="2012-12-06T10:29:53+00:00">
-                                <span class="day">06</span>
-                                <span class="mounth">Dec.</span>
-                                <span class="time">10:29 am</span>
-                            </time>
-
-
-                            <div class="entry-content">
-                                <a href='' class="title">Meet Joann Davidson: photomodel, athlete, excellent wife and beautiful woman</a>
-
-                                <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at,... ... </p>
-                            </div>
-                        </article>
-
-
-
-                        <article class="mini date">
-
-                            <time datetime="2012-12-06T10:28:54+00:00">
-                                <span class="day">06</span>
-                                <span class="mounth">Dec.</span>
-                                <span class="time">10:28 am</span>
-                            </time>
-
-
-                            <div class="entry-content">
-                                <a href='' class="title">Video post example (not in slider)</a>
-
-                                <p> http://vimeo.com/35396305
-
-                                    &nbsp;
-
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at,... ... </p>
-                            </div>
-                        </article>
-
-
-
-                        <article class="mini date">
-
-                            <time datetime="2012-12-05T14:02:09+00:00">
-                                <span class="day">05</span>
-                                <span class="mounth">Dec.</span>
-                                <span class="time">2:02 pm</span>
-                            </time>
-
-
-                            <div class="entry-content">
-                                <a href='' class="title">Gallery post example (Not in slider)</a>
-
-                                <p> 
-
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at,... ... </p>
-                            </div>
-                        </article>
-
-
-                </div></section>    </div>
-
-            <div class="five columns">
-
-
-                <section id="facebook_widget-2" class="widget-1 widget-first widget widget_facebook_widget"><div class="widget-inner"><div class="subtitle">Integrated facebook widget</div><h3>Facebook widget</h3>
-
-
-
-                    
-
-                        <div id="fb-root"></div>
-
-                       
-
-                </div></section>    </div>
-        </div>
-
-        <div class="row dop-row">
-            <div class="five columns">
-
-                <section class="footer-logo">
-                   <h1> <a href="#">UK Open College</a></h1>
-                </section>
-
+  <section id="footer" role="contentinfo">
+    <div class="row">
+      <div class="five columns">
+        <section id="crum_latest_tweets-2" class="widget-1 widget-first widget twitter-widget">
+          <div class="widget-inner">
+            <div class="subtitle">Our latest twitter news</div>
+            <h3>Latest Tweets</h3>
+            <div class='tweet'>Crumina: Check this! Great #themeforest item 'One Touch - Multifunctional Metro Stylish Theme'
+              <div class='time'>77 days ago</div>
             </div>
-
-
-            <div class="five columns">
-
-                <section class="widget widget_info">
-                    <div class="info-widget">
-                        <div class="subtitle">IT IS REALLY INTERESTING</div>
-                        <h3>About company</h3>
-
-                        <p>I'm not sure that anyone saw this comming,but now that its here, its make goood sense:Parents have smart phones. Some of their chil-</p>
-
-                    </div>
-                </section>
-
+            <div class='tweet'>Crumina: BGs
+              <div class='time'>105 days ago</div>
             </div>
-
-            <div class="five columns">
-
-                <section class="adress-icon">
-                    <div data-picture data-alt="adress-icon" class="inline-icon">
-                        <div data-src="<?php echo theme_img("icons/map_w.png");?>"></div>
-                        <div data-src="<?php echo theme_img("icons/map_w@2x.png");?>" data-media="(min-width: 400px) and (min-device-pixel-ratio: 1.5)"></div>
-
-                        <!--[if (lt IE 9) & (!IEMobile)]>
+            <div class='tweet'>Crumina: Check this! Great creative Photo and video WP Theme - Boson.
+              <div class='time'>111 days ago</div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div class="five columns">
+        <section id="recent_posts-2" class="widget-1 widget-first widget recent-posts-widget">
+          <div class="widget-inner">
+            <div class="subtitle"> Some latest news</div>
+            <h3>Recent posts</h3>
+            <article class="mini date">
+              <time datetime="2012-12-06T10:29:53+00:00"> <span class="day">06</span> <span class="mounth">Dec.</span> <span class="time">10:29 am</span> </time>
+              <div class="entry-content"> <a href='' class="title">Meet Joann Davidson: photomodel, athlete, excellent wife and beautiful woman</a>
+                <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at,... ... </p>
+              </div>
+            </article>
+            <article class="mini date">
+              <time datetime="2012-12-06T10:28:54+00:00"> <span class="day">06</span> <span class="mounth">Dec.</span> <span class="time">10:28 am</span> </time>
+              <div class="entry-content"> <a href='' class="title">Video post example (not in slider)</a>
+                <p> http://vimeo.com/35396305
+                  
+                  &nbsp;
+                  
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at,... ... </p>
+              </div>
+            </article>
+            <article class="mini date">
+              <time datetime="2012-12-05T14:02:09+00:00"> <span class="day">05</span> <span class="mounth">Dec.</span> <span class="time">2:02 pm</span> </time>
+              <div class="entry-content"> <a href='' class="title">Gallery post example (Not in slider)</a>
+                <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at,... ... </p>
+              </div>
+            </article>
+          </div>
+        </section>
+      </div>
+      <div class="five columns">
+        <section id="facebook_widget-2" class="widget-1 widget-first widget widget_facebook_widget">
+          <div class="widget-inner">
+            <div class="subtitle">Integrated facebook widget</div>
+            <h3>Facebook widget</h3>
+            <div id="fb-root"></div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <div class="row dop-row">
+      <div class="five columns">
+        <section class="footer-logo">
+          <h1> <a href="#">UK Open College</a></h1>
+        </section>
+      </div>
+      <div class="five columns">
+        <section class="widget widget_info">
+          <div class="info-widget">
+            <div class="subtitle">IT IS REALLY INTERESTING</div>
+            <h3>About company</h3>
+            <p>I'm not sure that anyone saw this comming,but now that its here, its make goood sense:Parents have smart phones. Some of their chil-</p>
+          </div>
+        </section>
+      </div>
+      <div class="five columns">
+        <section class="adress-icon">
+          <div data-picture data-alt="adress-icon" class="inline-icon">
+            <div data-src="<?php echo theme_img("icons/map_w.png");?>"></div>
+            <div data-src="<?php echo theme_img("icons/map_w@2x.png");?>" data-media="(min-width: 400px) and (min-device-pixel-ratio: 1.5)"></div>
+            <!--[if (lt IE 9) & (!IEMobile)]>
                         <div data-src="http://theme.crumina.net/onetouch/wp-content/themes/theme_v_1.tmp/assets/icons/map_w.png"></div>
                         <![endif]-->
-
-                        <!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
-                        <noscript>
-                            <img src="<?php echo theme_img("custom-slider-img/map_w.png");?>" alt="adress-icon" class="adress-icon">
-                        </noscript>
-                    </div>
-
-
-                    <p>Address:   123456 Street Name, Los Angeles <br>
-                        Phone:   (1800) 765-4321</p>
-                </section>
-
-            </div>
-        </div>
-    </section>
-
-        </div>
-         <a href="#" id="linkTop" class="backtotop">
-            <span></span>
-        </a>
-              
-              <?php echo theme_css('farbtastic1.3.css', true); ?>
-              <?php echo theme_css('grid.css', true); ?>
-              <?php //echo theme_css('farbtastic.css', true); ?>
-              <?php //echo theme_css('custom_style.css', true); ?>
-              <?php echo theme_js('js/jquery-plugins.min.js', true);?> 
-              <?php echo theme_js('js/aqpb-view.js', true);?>
-              <?php echo theme_js('js/add-to-cart.min.js', true);?>         
-              <?php echo theme_js('js/jquery.ui.core.min.js', true);?>     
-              <?php echo theme_js('js/jquery.ui.widget.min.js', true);?>
-              <?php echo theme_js('js/jquery.ui.tabs.min.js', true);?> 
-              <?php echo theme_js('js/jquery-ui-tabs-rotate.js', true);?> 
-              <?php echo theme_js('js/js_composer_front.js', true);?> 
-              <?php echo theme_js('js/jquery.isotope.min.js', true);?> 
-              <?php echo theme_js('js/metro-list.js', true);?>  
-              <?php echo theme_js('js/jquery.easing.1.3.js', true);?> 
-              <?php echo theme_js('js/jquery.nicescroll.js', true);?> 
-              <?php echo theme_js('js/jquery.colorbox.js', true);?>        
-              <?php echo theme_js('js/jflickrfeed.min.js', true);?> 
-              <?php echo theme_js('js/site.js', true);?> 
-              <?php echo theme_js('js/farbtastic.js', true);?>
-              <?php echo theme_js('js/custom_style.js', true);?>         
-              <?php echo theme_js('js/jquery.quicksand.js', true);?>  
-              <?php //echo theme_js('js/woocommerce.min.js', true);?>  
-              <?php echo theme_js('js/jquery.prettyPhoto.js', true);?> 
-              <?php //echo theme_js('js/woocommerce.min.js', true);?>
-              <?php echo theme_js('js/jquery.masonry.min.js', true);?>  
-               
-    </body>
-</html>
+            <!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+            <noscript>
+            <img src="<?php echo theme_img("custom-slider-img/map_w.png");?>" alt="adress-icon" class="adress-icon">
+            </noscript>
+          </div>
+          <p>Address:   123456 Street Name, Los Angeles <br>
+            Phone:   (1800) 765-4321</p>
+        </section>
+      </div>
+    </div>
+  </section>
+</div>
+<a href="#" id="linkTop" class="backtotop"> <span></span> </a> <?php echo theme_css('farbtastic1.3.css', true); ?> <?php echo theme_css('grid.css', true); ?>
+<?php //echo theme_css('farbtastic.css', true); ?>
+<?php //echo theme_css('custom_style.css', true); ?>
+<?php echo theme_js('js/jquery-plugins.min.js', true);?> <?php echo theme_js('js/aqpb-view.js', true);?> <?php echo theme_js('js/add-to-cart.min.js', true);?> <?php echo theme_js('js/jquery.ui.core.min.js', true);?> <?php echo theme_js('js/jquery.ui.widget.min.js', true);?> <?php echo theme_js('js/jquery.ui.tabs.min.js', true);?> <?php echo theme_js('js/jquery-ui-tabs-rotate.js', true);?> <?php echo theme_js('js/js_composer_front.js', true);?> <?php echo theme_js('js/jquery.isotope.min.js', true);?> <?php echo theme_js('js/metro-list.js', true);?> <?php echo theme_js('js/jquery.easing.1.3.js', true);?> <?php echo theme_js('js/jquery.nicescroll.js', true);?> <?php echo theme_js('js/jquery.colorbox.js', true);?> <?php echo theme_js('js/jflickrfeed.min.js', true);?> <?php echo theme_js('js/site.js', true);?> <?php echo theme_js('js/farbtastic.js', true);?> <?php echo theme_js('js/custom_style.js', true);?> <?php echo theme_js('js/jquery.quicksand.js', true);?>
+<?php //echo theme_js('js/woocommerce.min.js', true);?>
+<?php echo theme_js('js/jquery.prettyPhoto.js', true);?>
+<?php //echo theme_js('js/woocommerce.min.js', true);?>
+<?php echo theme_js('js/jquery.masonry.min.js', true);?>
+</body></html>
