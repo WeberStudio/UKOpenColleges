e4415bf1cbc32173c0c6bddf344853908628ae4a
junaid <j.khalil@weprosolutions.co.uk>
2013-05-28 18:11:14 +0500

    create fourms and file manager

---
+++ gocart/themes/oneTouch/views/dashboard_file_manager.php.REMOTE.php
@@ -0,0 +1,49 @@
+ <?php include('mainHeader.php'); ?>
+  <?php echo theme_css('basee.css', true); ?> 
+<body class="home page page-template page-template-page-no_top-php theme-onetouch wpb-js-composer js-comp-ver-3.4.12 vc_responsive">
+        <script> var customStyleImgUrl = "images/custom-slider-img";</script>
+          <?php //include('leftPanel.php'); ?>
+        <a href="#"><img id="nominee" src="<?=theme_img("custom-slider-img/cssreel_nomineetag_yellow.png")?>" alt="" /></a>
+		
+        <div id="body-wrapper" >		
+            <div id="body-wrapper-padding">			
+                <!--[if lt IE 7]>
+                <div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different
+                browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to
+                experience this site.
+                </div><![endif]-->
+                 <?php include('header.php'); ?> 
+                 <div class="line"> </div>
+               
+<div id="main">
+  <div class="container">
+        
+    <div id="main_container">
+      <div class="row-fluid">
+        <?php include('dashboard_leftpanal.php');?>
+        <!-- End .span3 -->
+        
+        <div class="span9">
+          
+          <!-- End .legend -->
+          
+          file manager
+          <!-- End .content --> 
+        </div>
+        <!-- End .span9 --> 
+      </div>
+      <!-- End .row-fluid --> 
+      
+    </div>
+    <!-- End #container --> 
+  </div>
+ 
+</div>
+            </div>
+        </div>
+        <?php include_once('footer.php'); ?> 
+        
+       
+    </body>
+</html>
+
