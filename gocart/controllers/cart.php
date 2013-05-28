344bb3bc063bc49527c09c6088120986251da048
shahid <shahid1376@gmail.com>
2013-05-28 10:24:09 +0500

    revert file code

--- gocart/controllers/cart.php
+++ gocart/controllers/cart.php
@@ -22,7 +22,7 @@ class Cart extends Front_Controller {
 		$data['homepage']			= true;
         $data['allProduct']         = $this->Product_model->get_products_catogery_wise();    
 	   // echo '<pre>';print_r($data['allProduct']);
-       $this->load->view('homepage', $data);
+       $this->load->view('index', $data);
 	}
     function   allcourses()
     {
