6eec57b2326a35e5109060e871b90dcfcd85f77e
Junaid Khalil <j.khalil@weprosolutions.co.uk>
2013-05-31 15:20:20 +0500

    remove the extra css images

--- gocart/themes/oneTouch/views/category.php
+++ gocart/themes/oneTouch/views/category.php
@@ -2,7 +2,7 @@
 
 <body class="home page page-template page-template-page-no_top-php theme-onetouch wpb-js-composer js-comp-ver-3.4.12 vc_responsive">
 
-<a href="#"><img id="nominee" src="<?=theme_img("custom-slider-img/cssreel_nomineetag_yellow.png")?>" alt="" /></a>
+
 
 <div id="body-wrapper" >
 
