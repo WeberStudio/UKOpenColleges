55c45e52c1a01dcb7b539fa63b695d2a57db1beb
junaid <j.khalil@weprosolutions.co.uk>
2013-06-21 16:22:31 +0500

    empty image replacement

--- gocart/themes/oneTouch/views/dashboard_leftpanal.php
+++ gocart/themes/oneTouch/views/dashboard_leftpanal.php
@@ -9,7 +9,8 @@
 			  if(!empty($this->customer['tutor_id'])){
 			  
 			   //print_r($tutor_details);exit; 
-			  	$get_address 	= $this->Tutor_model->get_address($user_info['customer']['tutor_id']);                
+			  	$get_address 	= $this->Tutor_model->get_address($user_info['customer']['tutor_id']);
+				$get_address_cus  = "";                
 			    echo  $user_info['customer']['firstname']." ". $user_info['customer']['lastname'];      
 			  }
 			  //if($this->Customer_model->is_logged_in(false, false))
@@ -17,6 +18,7 @@
 			  {
 				  
 				  $get_address_cus  = $this->Customer_model->get_address_pro($user_info['customer']['id']);
+				  $get_address = "";
 				  echo  $user_info['customer']['firstname']." ". $user_info['customer']['lastname'];
 			  }
 			  
@@ -27,7 +29,16 @@
           </div>
           <!-- End .title -->
           <div class="content">
-            <div class="row-fluid well well-small"> <img class="row-fluid" src= "<?php if(!empty($get_address)){ echo base_url().'uploads/images/full/'.$get_address[0]->avatar;} if(!empty($get_address_cus)){ echo base_url().'uploads/images/full/'.$get_address_cus[0]->image;}
+            <div class="row-fluid well well-small">
+            
+             <img class="row-fluid" src= "<?php
+			  if(!empty($get_address) && $get_address[0]->avatar!="" ){ echo base_url().'uploads/images/full/'.$get_address[0]->avatar;}
+			  if(!empty($get_address_cus) && $get_address_cus[0]->image!=""){ echo base_url().'uploads/images/full/'.$get_address_cus[0]->image;} 
+			  if(!empty($get_address_cus))
+			  {if($get_address_cus[0]->image==""){echo theme_img('avatar.png');}}
+			  
+			  if(!empty($get_address))
+			  {if($get_address[0]->avatar==""){echo theme_img('avatar.png');}}
 			?>"> </div>
             <ul class="nav1 nav-tabs dark nav-stacked">
             <?php if(!empty($this->customer['tutor_id'])):?>
